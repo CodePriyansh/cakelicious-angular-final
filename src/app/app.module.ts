1e60dca82aa6763311ef8f5e1697028b5ca9cf65
mohitibfoundation <mohit.ibfoundation@gmail.com>
2022-05-12 19:04:38 +0530

    signin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,27 @@ import { HomeComponent } from './user/home/home.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainContentComponent } from './user/main-content/main-content.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule ,HTTP_INTERCEPTORS } from '@angular/common/http'
+import { ToastrModule } from 'ngx-toastr';
+import { CacheInterceptorsService } from './interceptors/cache-interceptors.service';
+import { InterceptorService } from './interceptors/interceptor.service';
+import { SocialLoginModule , GoogleLoginProvider  } from 'angularx-social-login';
+
+
+const socialProvider={
+  provide:"SocialAuthServiceConfig",
+  useValue:{
+    providers:[{
+      id:GoogleLoginProvider.PROVIDER_ID,
+      provider:new GoogleLoginProvider("150365577052-cv0ao56rbs5t7ojg3lnirmpe5qpebuqn.apps.googleusercontent.com")
+      
+    }]
+  }
+};
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,9 +42,23 @@ import { MainContentComponent } from './user/main-content/main-content.component
     BrowserModule,
     AppRoutingModule,
     CarouselModule ,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    SocialLoginModule,
+    ToastrModule.forRoot()
+
   ],
-  providers: [],
+  providers: [socialProvider,{
+    provide : HTTP_INTERCEPTORS,
+    useClass : InterceptorService,
+    multi : true
+  },
+{
+  provide:HTTP_INTERCEPTORS,
+  useClass: CacheInterceptorsService,
+  multi : true
+}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/user/signup/signup.component.ts
+++ src/app/user/signup/signup.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { LoginService } from 'src/app/services/login.service';
+import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  user: User = new User('', '', '', '');
+  constructor(
+    private service: LoginService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public signUp() {
+    this.service.signUp(this.user).subscribe((data: any) => {
+      this.toastr.success(
+        'Congratulations :' +
+          data.name +
+          ', Your account has been created successfully, Please check your inbox to activate your account..'
+      );
+      this.router.navigate(['/signin']);
+    });
   }
 
+
+  ngOnInit(): void {}
 }
